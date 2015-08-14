5fcf9ca154994b9aeb8ca7c824fad69bc8650b7d
Sandeep Gupta <sgupta654@gmail.com>
2015-08-14 11:40:26 -0400

    Initial Commit

---
+++ myFirstApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myFirstApplication
+//
+//  Created by Gupta, Sandeep on 8/14/15.
+//  Copyright (c) 2015 Gupta, Sandeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myFirstApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myFirstApplication
+//
+//  Created by Gupta, Sandeep on 8/14/15.
+//  Copyright (c) 2015 Gupta, Sandeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
